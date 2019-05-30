b087a5f0d1664508156dce4892b642e165fe3a63
Safe_Mode <safe2mode@gmail.com>
2019-05-30 18:40:56 +0300

    Fix errors on edit page

--- view/article_admin.php
+++ view/article_admin.php
@@ -25,17 +25,17 @@
       <form method="POST" action="index.php?action=<?=$_GET['action']?>&id=<?=$_GET['id']?>">
         <div class="form-group">
           <label for="add-title">Заголовок</label>
-          <input type="text" class="form-control" id="add-title" name="title" value="<?=$article['title']?>" placeholder="Заголовок статьи" required>
+          <input type="text" class="form-control" id="add-title" name="title" value="<?=(isset($article)) ? $article['title'] : ''?>" placeholder="Заголовок статьи" required>
         </div>
 
         <div class="form-group">
           <label for="add-date">Дата</label>
-          <input type="date" class="form-control" id="add-date" name="date" value="<?=$article['date']?>" placeholder="Дата публикации" required>
+          <input type="date" class="form-control" id="add-date" name="date" value="<?=(isset($article)) ? $article['date'] : ''?>" placeholder="Дата публикации" required>
         </div>
 
         <div class="form-group">
           <label for="add-content">Контент</label>
-          <textarea class="form-control" id="add-content" rows="5" name="content" placeholder="Текст статьи" required><?=$article['content']?></textarea>
+          <textarea class="form-control" id="add-content" rows="5" name="content" placeholder="Текст статьи" required><?=(isset($article)) ? $article['content'] : ''?></textarea>
         </div>
 
         <button type="submit" class="btn btn-success">Сохранить</button>
