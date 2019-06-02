cdcaad500d3c9838f23a038168088e1e32c87f7b
Safe_Mode <safe2mode@gmail.com>
2019-06-02 17:19:54 +0300

    Add image to article details

--- view/article.php
+++ view/article.php
@@ -4,9 +4,9 @@ require_once('assets/header.php');
 
 <article class="page__article article">
   <header class="article__header">
-    <!-- <img class="article__image" src="<?= $article['image']['src'] ?>" alt="<?= $article['image']['alt'] ?>"> -->
     <h1 class="article__title"><?= $article['title'] ?></h1>
     <p class="article__meta">Опубликовано: <time datetime="<?= $article['date'] ?>"><?= $article['date'] ?></time></p>
+    <img class="article__image" src="<?= $article['image'] ?>" alt="<?= $article['title'] ?>">
   </header>
 
   <div class="article__body"><?= $article['content'] ?></div>
